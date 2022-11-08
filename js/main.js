7c525f90f45d2b572ddb06a02adf857f49635295
luciobo <luciobo@icloud.com>
2022-11-08 16:27:49 +0100

    creato controllo palindromo non funzionante

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 
 
+let parola = "osso"
+console.log(parola)
+
+let parolaDivisa = parola.split('')
+console.log(parolaDivisa)
+
+
+let parolainvertita = parolaDivisa.reverse()
+console.log(parolainvertita)
+
+let parolaRicomposta = parolainvertita.join()
+console.log (parolaRicomposta)
+
+
+if (parolaDivisa === parolaRicomposta) {
+    console.log ('questo è un palindromo')
+} else {
+    console.log ('questo non è un palindromo')
+}
